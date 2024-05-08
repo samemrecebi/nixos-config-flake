ca9fc294e553d005a7144f0998d6cf1fd5114a23
Emre Cebi <emre@cebi.io>
2024-05-08 17:39:28 +0200

    adding more dotfiles

--- home-manager/mbp/home.nix
+++ home-manager/mbp/home.nix
@@ -17,9 +17,9 @@
   programs.starship.enable = true;
 
   home.file.".gitconfig".source = ../../dotfiles/.gitconfig;
-  home.file.".config/starship.toml".source = ../../dotfiles/config/starship.toml;
-  home.file.".config/alacritty".source = ../../dotfiles/config/alacritty;
-  home.file.".emacs.d/init.el".source = ../../dotfiles/emacs.d/init.el;
-  home.file.".emacs.d/early-init.el".source = ../../dotfiles/emacs.d/early-init.el;
+  home.file.".config/starship.toml".source = ../../dotfiles/.config/starship.toml;
+  home.file.".config/alacritty".source = ../../dotfiles/.config/alacritty;
+  home.file.".emacs.d/init.el".source = ../../dotfiles/.emacs.d/init.el;
+  home.file.".emacs.d/early-init.el".source = ../../dotfiles/.emacs.d/early-init.el;
 }
  
