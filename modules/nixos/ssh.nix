70d606fe0fdf4fe93bc6a060b68ae81890e70117
Emre Cebi <emre@cebi.io>
2024-05-13 16:27:59 +0200

    NEW MODULE BABY

---
+++ modules/nixos/ssh.nix
@@ -0,0 +1,37 @@
+{
+  pkgs,
+  options,
+  ...
+}: {
+  #SSH
+  programs.ssh = {
+    startAgent = true;
+  };
+  systemd.user.services.add-ssh-key = {
+    inherit (config.systemd.user.services.ssh-agent) unitConfig wantedBy;
+    bindsTo = [
+      "ssh-agent.service"
+    ];
+    environment.SSH_AUTH_SOCK = "/run/user/%U/ssh-agent";
+    path = [
+      options.programs.ssh.package.value
+    ];
+    script = "${options.programs.ssh.package.value}/bin/ssh-add";
+    serviceConfig = {
+      CapabilityBoundingSet = "";
+      LockPersonality = true;
+      NoNewPrivileges = true;
+      ProtectClock = true;
+      ProtectHostname = true;
+      PrivateNetwork = true;
+      ProtectKernelLogs = true;
+      ProtectKernelModules = true;
+      ProtectKernelTunables = true;
+      RestrictAddressFamilies = "AF_UNIX";
+      RestrictNamespaces = true;
+      SystemCallArchitectures = "native";
+      SystemCallFilter = "~@clock @cpu-emulation @debug @module @mount @obsolete @privileged @raw-io @reboot @resources @swap";
+      UMask = "0777";
+    };
+  };
+}
