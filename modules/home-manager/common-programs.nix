88edc95d54f334077cd971d1492ac4c36e3f3479
Emre Cebi <emre@cebi.io>
2024-05-14 23:52:26 +0200

    Created a common programs module

---
+++ modules/home-manager/common-programs.nix
@@ -0,0 +1,33 @@
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    # General Packages
+    neofetch
+    htop
+    man
+    sl
+    wget
+    curl
+    pciutils
+    libtool
+    yt-dlp
+    hugo
+    imagemagick
+    ispell
+    gnupg
+    pandoc
+    yubikey-agent
+    wget
+    gnupg
+
+    # LaTeX
+    texliveMedium
+    
+    # Dev Packages
+    gcc
+    clang-tools
+    nixpkgs-fmt
+    nil
+    nodejs
+    nodePackages.npm
+  ];
+}
