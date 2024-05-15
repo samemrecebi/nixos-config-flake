882b4ea07aee1f6724ca510b067ac311edbf3d23
Emre Cebi <emre@cebi.io>
2024-05-15 10:21:10 +0200

    fixed NIL

--- modules/home-manager/home-shell.nix
+++ modules/home-manager/home-shell.nix
@@ -12,13 +12,13 @@
       em = "emacsclient -c -n -a ''";
       updatesys = "darwin-rebuild switch --flake ~/.nix";
     };
-    history.size = 10000;
-    history.path = "${config.xdg.dataHome}/zsh/history";
     sessionVariables = {
       DIRENV_LOG_FORMAT = ""; # silence direnv
       EDITOR = "code";
       NIXPKGS_ALLOW_UNFREE = "1";
     };
+    history.size = 10000;
+    history.path = "${config.xdg.dataHome}/zsh/history";
   };
 
   programs.starship.enable = true;
