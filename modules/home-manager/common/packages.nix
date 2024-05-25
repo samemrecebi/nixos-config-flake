b27094a52b0f4b03c968ba76a953809277944801
Emre Cebi <emre@cebi.io>
2024-05-25 11:58:41 +0200

    More new stuff

--- modules/home-manager/common/packages.nix
+++ modules/home-manager/common/packages.nix
@@ -1,7 +1,21 @@
-{pkgs, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: let
+  tex = pkgs.texlive.combine {
+    inherit
+      (pkgs.texlive)
+      scheme-medium
+      fontawesome5
+      multirow
+      moderncv
+      beamer
+      ;
+  };
+in {
   home.packages = with pkgs; [
-    # LaTeX
-    texliveMedium
+    tex
 
     # Nix Related Packages
     alejandra
