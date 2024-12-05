8c42e7696b8d07d05536329937e84d64e28b06a2
Emre Cebi <emre@cebi.io>
2024-12-05 23:43:28 +0100

    idontknowwhatimdoing

--- home-manager/common/fonts.nix
+++ home-manager/common/fonts.nix
@@ -1,10 +1,9 @@
-{pkgs, ...}: {
+{pkgs}: {
   home.packages = [
     pkgs.noto-fonts
     pkgs.nerd-fonts.fira-code
     pkgs.nerd-fonts.hack
     pkgs.nerd-fonts.roboto-mono
     pkgs.nerd-fonts.jetbrains-mono
-    pkgs.berkeley-mono
   ];
 }
